0592307f0db1c90205cbd04f2e7d14be65d17917
Phan Anh Tú <124888378+tuphan22028238@users.noreply.github.com>
2023-06-09 00:50:34 +0700

    Add files via upload

--- 10. Approximating Eigenvalue/Power_method_Euclid.cpp
+++ 10. Approximating Eigenvalue/Power_method_Euclid.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
     int n;
-    cout << "Nhap ma tran cap: ";
+    cout << "Enter the order of matrix: ";
     cin >> n;
     double c;
-    cout << "Nhap so lan hoi tu: ";
+    cout << "Enter the number of iterations: ";
     cin >> c;
     double truc_giao_i = 0;
     double di;
@@ -16,18 +16,18 @@ int main()
     double a[20][20];
     double b[20][1];
     double lamda = 0;
-    cout << "Nhap ma tran A: " << endl;
-    for(int i=0;i<n;i++)
+    cout << "Enter the matrix A: " << endl;
+    for(int i=0; i<n; i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0; j<n; j++)
         {
             cin >> a[i][j];
         }
     }
-    cout << "Nhap vector don vi: " << endl;
-    for(int i=0;i<n;i++)
+    cout << "Enter initial unit vector: " << endl;
+    for(int i=0; i<n; i++)
     {
-        for(int j=0;j<1;j++)
+        for(int j=0; j<1; j++)
         {
             cin >> b[i][j];
         }
@@ -36,65 +36,72 @@ int main()
 
     while(c--)
     {
-        for(int i=0;i<n;i++)
+        for(int i=0; i<n; i++)
         {
-            for(int j=0;j<1;j++)
+            for(int j=0; j<1; j++)
             {
                 y[i][j] = 0;
             }
         }
-        for(int i=0;i<n;i++)
+        
+		for(int i=0; i<n; i++)
         {
-            for(int j=0;j<1;j++){
-
-            for(int k=0;k<n;k++)
-            {
-                y[i][j] += a[i][k] * b[k][j];
-            }
-        }
-    }
-    truc_giao_i = 0;
-     for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<1;j++)
-        {
-            truc_giao_i += pow(y[i][j], 2);
-        }
-    }
-    di = sqrt(truc_giao_i);
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<1;j++)
-        {
-            x0[i][j] = y[i][j]/di;
-        }
-    }
-    lamda = 0;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<1;j++)
-        {
-            lamda += y[j][i] * x0[i][j];
-        }
-    }
-     for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<1;j++)
-        {
-            b[i][j] = x0[i][j];
-        }
-    }
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<1;j++)
-        {
-            cout << setprecision(15) << fixed << x0[i][j] << ' ';
-        }
-        cout << endl;
-    }
-    cout << "Gia tri rieng lamda la: ";
-    cout << setprecision(15) << fixed << lamda << endl;
-    cout << endl;
-    }
+	        for(int j=0; j<1; j++)
+			{
+	            for(int k=0; k<n; k++)
+	            {
+	                y[i][j] += a[i][k] * b[k][j];
+	            }
+	        }
+	    }
+	    
+		truc_giao_i = 0;
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<1;j++)
+	        {
+	            truc_giao_i += pow(y[i][j], 2);
+	        }
+	    }
+	    
+		di = sqrt(truc_giao_i);
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<1;j++)
+	        {
+	            x0[i][j] = y[i][j]/di;
+	        }
+	    }
+	    
+		lamda = 0;
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<1;j++)
+	        {
+	            lamda += y[j][i] * x0[i][j];
+	        }
+	    }
+	    
+		for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<1;j++)
+	        {
+	            b[i][j] = x0[i][j];
+	        }
+	    }
+	    
+		for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<1;j++)
+	        {
+	            cout << setprecision(15) << fixed << x0[i][j] << ' ';
+	        }
+	        cout << endl;
+	    }
+	    
+		cout << "The dominant eigenvalue: ";
+	    cout << setprecision(15) << fixed << lamda << endl;
+	    cout << endl;
+	}
 }
 
